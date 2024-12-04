274c79ca4871c70cfd0fb83f20f9cc19d3d22bc8
Yunus <yrherman8@gmail.com>
2024-12-04 12:31:28 -0500

    adding several files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Unit-1-browser-based-game-project
+✅ More than two items marked incomplete in this section will require you to use your one redo to re-submit them. If two or fewer items are marked incomplete, the project is considered passing. Your README must contain the items or sections below.
+
+Screenshot/Logo: A screenshot of your app or a logo.
+Your game’s name: Include a description of your game and what it does. Background info about the game and why you chose it is a nice touch.
+Getting started: Include a link to your deployed game and any instructions you deem important. This should also contain a link to your planning materials.
+Attributions: This section should include links to any external resources (such as libraries or assets) you used to develop your application that require attribution. You can exclude this section if it does not apply to your application.
+Technologies used: List of the technologies used, for example: JavaScript, HTML, CSS, etc.
+Next steps: Planned future enhancements (stretch goals).
+
+![alt text](image.png)
--- js/app.js
+++ js/app.js
@@ -11,10 +11,89 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
+const handleBoxClick = (event) => {
+    if (winner === true) {
+      return;
+    }
+    const squareIndex = event.target.id;
+  
+  
+    if (board[squareIndex] !== '') {
+      return;
+    }
+  
+    board[squareIndex] = turn
+  
+    updateBoard();
+    checkForWinner();
+    updateMessage();
+    switchPlayerTurn();
+   
+  }
 
+
+const updateBoard = () => {
+    console.log("Update board called");
+  
+    //['', '', '', '', '', '', '', '', ''];
+  
+    road1.forEach((element, index) => {
+  
+      boxes[index].textContent = element
+    })
+  
+  }
+  
+  const updateMessage = () => {
+    console.log("Update message called");
+  
+    if (winner === false && tie === false) {
+      if (turn === 'X') {
+        messageEl.textContent = `Its X's turn`;
+      } else {
+        messageEl.textContent = `Its O's turn`;
+      }
+    } else if (winner === true) {
+      messageEl.textContent = `${turn} wins!`;
+    }
+  }
+
+const render = () => {
+    console.log("Rendering board", board);
+    updateBoard();
+    updateMessage();
+}
+
+const init = () => {
+    road1 = ['0','1','2','3','4','5','6','7','8','9'];
+    turn = 'X';
+    winner = false;
+    die = false;
+
+    
+}
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
+// const elements = document.querySelectorAll('.road-box');
 
+// elements.forEach((element, index) => {
+//   setTimeout(() => {
+//     // Apply your effect here, e.g., change class or style
+//     element.classList.add('visible'); 
+//   }, index * 500); // Delay in milliseconds, increasing for each element
+// });
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowDown') {
+      // Your code to execute when the down arrow key is pressed
+      console.log('Down arrow key pressed!');
+    }
+  });
+
+const imageElement = document.querySelector('#roadbox0');
+console.dir(imageElement);
+
+imageElement.innerHTML = '<img src="./images/right.png" alt="">'; 
